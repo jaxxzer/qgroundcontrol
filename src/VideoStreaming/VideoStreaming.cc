46dcdd188132a5527d6afac2d452dcb3834dc5e9
Jacob Walser <jwalser90@gmail.com>
2017-06-12 18:03:45 -0400

    GST_DEBUG level 4

--- src/VideoStreaming/VideoStreaming.cc
+++ src/VideoStreaming/VideoStreaming.cc
@@ -56,30 +56,39 @@ static void qgcputenv(const QString& key, const QString& root, const QString& pa
 #endif
 #endif
 
-#ifdef ANDDROID_GST_DEBUG
 // Redirects stdio and stderr to logcat
 #include <unistd.h>
 #include <pthread.h>
-#include <android/log.h>
 
 static int pfd[2];
 static pthread_t thr;
 static const char *tag = "myapp";
+QFile* file;
 
 static void *thread_func(void*)
 {
     ssize_t rdsz;
     char buf[128];
+
+
+
     while((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0) {
         if(buf[rdsz - 1] == '\n') --rdsz;
         buf[rdsz] = 0;  /* add null-terminator */
-        __android_log_write(ANDROID_LOG_DEBUG, tag, buf);
+        {
+            QTextStream stream(file);
+            stream << buf;
+        }
     }
     return 0;
 }
 
 int start_logger(const char *app_name)
 {
+    QString filename = QDir::homePath() + "/gstdebug.txt";
+    file = new QFile(filename);
+    file->open(QIODevice::WriteOnly);
+    qDebug() << filename;
     tag = app_name;
 
     /* make stdout line-buffered and stderr unbuffered */
@@ -87,9 +96,11 @@ int start_logger(const char *app_name)
     setvbuf(stderr, 0, _IONBF, 0);
 
     /* create the pipe and redirect stdout and stderr */
-    pipe(pfd);
+    int x = pipe(pfd);
     dup2(pfd[1], 1);
     dup2(pfd[1], 2);
+
+    qDebug() << x;
 
     /* spawn the logging thread */
     if(pthread_create(&thr, 0, thread_func, 0) == -1)
@@ -97,7 +108,6 @@ int start_logger(const char *app_name)
     pthread_detach(thr);
     return 0;
 }
-#endif
 
 void initializeVideoStreaming(int &argc, char* argv[])
 {
@@ -118,11 +128,9 @@ void initializeVideoStreaming(int &argc, char* argv[])
         qgcputenv("GST_PLUGIN_PATH", currentDir, "/gstreamer-plugins");
     #endif
         // Initialize GStreamer
-        #ifdef ANDDROID_GST_DEBUG
-        start_logger("gst_log");
+        start_logger("qgcvidebug");
         qputenv("GST_DEBUG", "*:4");
         qputenv("GST_DEBUG_NO_COLOR", "1");
-        #endif
         GError* error = NULL;
         if (!gst_init_check(&argc, &argv, &error)) {
             qCritical() << "gst_init_check() failed: " << error->message;
